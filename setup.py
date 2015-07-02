d09722335965d7f1e51752c1d6f83255f73a68cd
Chris Crook <ccrook@linz.govt.nz>
2015-07-03 10:18:41 +1200

    Fixing name of ellipsoid entry point

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'ellipsoid=LINZ.geodetic.ellipsoid:main',
+            'ellipsoid=LINZ.Geodetic.Ellipsoid:main',
         ],
     },
 )
