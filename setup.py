452e1414e36a23921becce9ec001b9ce10297122
Chris Crook <ccrook@linz.govt.nz>
2019-12-30 08:14:05 +1300

    Removing ellipsoid program from python2 to avoid clash with python3

--- setup.py
+++ setup.py
@@ -90,8 +90,5 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': [
-            'ellipsoid=LINZ.Geodetic.Ellipsoid:main',
-        ],
     },
 )
